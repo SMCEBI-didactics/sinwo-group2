a663b5abfe1c370edf1eee5de36024b8a1bc3a38
OliwierP <oliwier.pszeniczko@smcebi.edu.pl>
2022-01-27 16:07:29 +0100

    Uzupelniono dockerfile oraz poprawiono main.py modulu

--- Modules/MiejscaZerowe/MiejscaZerowe/main.py
+++ Modules/MiejscaZerowe/MiejscaZerowe/main.py
@@ -1,28 +1,50 @@
+from setuptools import setup
+
+setup(
+    name="MiejscaZerowe",
+    description="Zawiera funkcje do wyznaczania miejsc zerowych",
+    version="v1.0",
+    author="Oliwier Pszeniczko",
+    author_email="",
+    licence="MIT",
+    install_requires=["click"],
+    packages=['MiejscaZerowe'],
+    entry_points={
+        'console_scripts': [
+            'metoda_bisekcji = MiejscaZerowe.main:metoda_bisekcji',
+            'metoda_siecznych = MiejscaZerowe.main:metoda_siecznych',
+
+        ]
+    }
+)
+opszeniczko@INF-VM:~/Projekt2/sinwo-group2/Modules/MiejscaZerowe$ cd MiejscaZerowe/opszeniczko@INF-VM:~/Projekt2/sinwo-group2/Modules/MiejscaZerowe/MiejscaZerowe$ ls
+__init__.py  main.py
+opszeniczko@INF-VM:~/Projekt2/sinwo-group2/Modules/MiejscaZerowe/MiejscaZerowe$ cat main.py
 import click
 
 
 @click.command()
-@click.option('--fun', type=str, prompt="Podaj wzor funkcji(x jako argument)")
-@click.option('--a', type=int, prompt="Podaj poczatek rozpatrywanego zakresu")
-@click.option('--b', type=int, prompt="Podaj koniec rozpatrywanego zakresu")
+@click.option('--fun')
+@click.option('--a')
+@click.option('--b')
 def metoda_bisekcji(fun, a, b):
-    
+
     """
     Funkcja wyznaczająca miejsce zerowe metodą bisekcji
-    
+
     :param f: wzór funkcji rozpatrywanej pod kątem znalezienia miejsca zerowego
     :type: str
     :param a: poczatek rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
     :param b: koniec rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
-    
+
     :return: znalezione miejsce zerowe
     :rtype: int
-    
+
     """
 
-    
+
     assert all([((c.isalpha() and c == 'x') or not c.isalpha()) for c in fun]), "tylko x moze zostac uzyty jako argument"
     assert all([c != '^' for c in fun]), "potegowanie odbywa sie zgodnie z pythonem za pomocom operatorow **"
 #     assert isinstance(f(1), (int, float)), "Podane dane nie jest działaniem"
@@ -30,43 +52,43 @@ def metoda_bisekcji(fun, a, b):
 
     epsilon=0.01
     imax=25
-    
+
     a = int(a)
     b = int(b)
-    
+
     if a > b:
         a, b = b, a
-    
+
     fa = wylicz(fun, a)
-    
+
     if abs(fa) < epsilon:
         return a
-    
+
     fb = wylicz(fun, b)
-    
+
     if abs(fb) < epsilon:
         return b
-    
+
     if fa * fb > 0:
         return None
-    
+
     for i in range(imax):
-        
+
         c = (a + b) / 2
         fc = wylicz(fun, c)
-        
+
         if abs(b - a) < epsilon:
             break
-            
+
         if abs(fc) < epsilon:
             break
-        
+
         if fa * fc > 0:
             a, fa = c, fc
-            
+
         if fb * fc > 0:
             b, fb = c, fc
-    
+
     if c is None:
         print("Nie istnieje miejsce zerowe dla danego zakresu")
     else:
@@ -74,23 +96,23 @@ def metoda_bisekcji(fun, a, b):
     return c
 
 def _metoda_bisekcji(fun, a, b):
-    
+
     """
     Funkcja wyznaczająca miejsce zerowe metodą bisekcji
-    
+
     :param f: wzór funkcji rozpatrywanej pod kątem znalezienia miejsca zerowego
     :type: str
     :param a: poczatek rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
     :param b: koniec rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
-    
+
     :return: znalezione miejsce zerowe
     :rtype: int
-    
+
     """
 
-    
+
     assert all([((c.isalpha() and c == 'x') or not c.isalpha()) for c in fun]), "tylko x moze zostac uzyty jako argument"
     assert all([c != '^' for c in fun]), "potegowanie odbywa sie zgodnie z pythonem za pomocom operatorow **"
 #     assert isinstance(f(1), (int, float)), "Podane dane nie jest działaniem"
@@ -98,112 +120,112 @@ def _metoda_bisekcji(fun, a, b):
 
     epsilon=0.01
     imax=25
-    
+
     a = int(a)
     b = int(b)
-    
+
     if a > b:
         a, b = b, a
-    
+
     fa = wylicz(fun, a)
-    
+
     if abs(fa) < epsilon:
         return a
-    
+
     fb = wylicz(fun, b)
-    
+
     if abs(fb) < epsilon:
         return b
-    
+
     if fa * fb > 0:
         return None
-    
+
     for i in range(imax):
-        
+
         c = (a + b) / 2
         fc = wylicz(fun, c)
-        
+
         if abs(b - a) < epsilon:
             break
-            
+
         if abs(fc) < epsilon:
             break
-        
+
         if fa * fc > 0:
             a, fa = c, fc
-            
+
         if fb * fc > 0:
             b, fb = c, fc
-    
+
     return c
 
 
 @click.command()
-@click.option('--fun', type=str, prompt="Podaj wzor funkcji(x jako argument)")
-@click.option('--a', type=int, prompt="Podaj poczatek rozpatrywanego zakresu")
-@click.option('--b', type=int, prompt="Podaj koniec rozpatrywanego zakresu")
+@click.option('--fun')
+@click.option('--a')
+@click.option('--b')
 def metoda_siecznych(fun, a, b):
-    
+
     """
     Funkcja wyznaczająca miejsce zerowe metodą siecznych
-    
+
     :param f: wzór funkcji rozpatrywanej pod kątem znalezienia miejsca zerowego
     :type: str
     :param a: poczatek rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
     :param b: koniec rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
-    
+
     :return: znalezione miejsce zerowe
     :rtype: int/None
-    
+
     """
-    
+
     assert all([((c.isalpha() and c == 'x') or not c.isalpha()) for c in fun]), "tylko x moze zostac uzyty jako argument"
     assert all([c != '^' for c in fun]), "potegowanie odbywa sie zgodnie z pythonem za pomocom operatorow **"
 #     assert isinstance(f(1), (int, float)), "Podane dane nie jest działaniem"
     assert any([c == 'x' for c in fun]), "Brak argumentu"
-    
+
     epsilon=0.01
     imax=25
-    
+
     a = int(a)
     b = int(b)
-    
+
     if a > b:
         a, b = b, a
-    
-    
+
+
     fa = wylicz(fun, a)
-    
+
     if abs(fa) < epsilon:
         return a
-    
+
     fb = wylicz(fun, b)
     if abs(fb) < epsilon:
         return b
-    
+
     if fa * fb > 0:
         return None
-    
+
     for i in range(imax):
 
-        
+
         c = (a * fb - b * fa) / (fb - fa)
         fc = wylicz(fun, c)
-        
+
         if abs(b - a) < epsilon:
             break
-            
+
         if abs(fc) < epsilon:
             break
-        
+
         if fa * fc > 0:
             a, fa = c, fc
-            
+
         if fb * fc > 0:
             b, fb = c, fc
-    
+
     if c is None:
         print("Nie istnieje miejsce zerowe dla danego zakresu")
     else:
@@ -212,82 +234,82 @@ def metoda_siecznych(fun, a, b):
 
 
 def _metoda_siecznych(fun, a, b):
-    
+
     """
     Funkcja wyznaczająca miejsce zerowe metodą siecznych
-    
+
     :param f: wzór funkcji rozpatrywanej pod kątem znalezienia miejsca zerowego
     :type: str
     :param a: poczatek rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
     :param b: koniec rozpatrywanego zakresu pod kątem szukanego miejsca zerowego
     :type: int
-    
+
     :return: znalezione miejsce zerowe
     :rtype: int/None
-    
+
     """
-    
+
     assert all([((c.isalpha() and c == 'x') or not c.isalpha()) for c in fun]), "tylko x moze zostac uzyty jako argument"
     assert all([c != '^' for c in fun]), "potegowanie odbywa sie zgodnie z pythonem za pomocom operatorow **"
 #     assert isinstance(f(1), (int, float)), "Podane dane nie jest działaniem"
     assert any([c == 'x' for c in fun]), "Brak argumentu"
-    
+
     epsilon=0.01
     imax=25
-    
+
     a = int(a)
     b = int(b)
-    
+
     if a > b:
         a, b = b, a
-    
-    
+
+
     fa = wylicz(fun, a)
-    
+
     if abs(fa) < epsilon:
         return a
-    
+
     fb = wylicz(fun, b)
     if abs(fb) < epsilon:
         return b
-    
+
     if fa * fb > 0:
         return None
-    
+
     for i in range(imax):
 
-        
+
         c = (a * fb - b * fa) / (fb - fa)
         fc = wylicz(fun, c)
-        
+
         if abs(b - a) < epsilon:
             break
-            
+
         if abs(fc) < epsilon:
             break
-        
+
         if fa * fc > 0:
             a, fa = c, fc
-            
+
         if fb * fc > 0:
             b, fb = c, fc
-        
+
     return c
 
-            
+
 def wylicz(fun, x):
     """
     Funkcja pomocnicza wyznaczajaca wartosc funkcji podanej w postaci lancucha znakow w podanym punkcie
-    
-    :param fun: funkcja, której ma zostać wyznaczona wartość w punkcie  
+
+    :param fun: funkcja, której ma zostać wyznaczona wartość w punkcie
     :type: str
     :param x: argument funkcji, w którym ma zostać wyznaczona wartość funkcji
     :type: int
-    
+
     :return: wartość funkcji
     :rtype int
-    
+
     """
     wynik = eval(fun)
     return wynik
