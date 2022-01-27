f4737be52abe66b5cb1acc8ef9abeacf85286580
adamage2k <adammartyniak424@gmail.com>
2022-01-27 13:33:10 +0100

    Usunieto zbedne foldery/pliki

--- Modules/Liczby_pseudolosowe/Liczby_pseudolosowe/main.py
+++ Modules/Liczby_pseudolosowe/Liczby_pseudolosowe/main.py
@@ -24,28 +24,6 @@ def RandomNumberGeneratorGauss(bottom, top):
         
             return randomNumber
 
-# def _RandomNumberGeneratorGauss(bottom, top):
-#     """
-#     Funkcja generująca liczbe pseudolosową,
-#     oparta na rozkładzie Gaussa
-    
-#     Argumenty:
-#     bottom - dolna granica przedziału losowania liczby
-#     top - górna granica przedziału losowania liczby
-    
-#     Return:
-#     Funckja zwraca liczbę pseudolosową z podanego przedziału
-#     """
-#     bottom=float(bottom)
-#     top=float(top)
-#     while(True):
-#         mu=top-bottom
-#         sigma=top-bottom
-#         randomNumber=random.gauss(mu, sigma)
-#         print(randomNumber)
-#         if bottom<randomNumber<top:
-            
-#             return randomNumber
         
                       
 def RandomNumberGeneratorUniform(bottom ,top):
@@ -65,22 +43,6 @@ def RandomNumberGeneratorUniform(bottom ,top):
     randomNumber=random.uniform(bottom, top)
     return randomNumber
 
-# def _RandomNumberGeneratorUniform(bottom ,top):
-#     """
-#     Funkcja generująca liczbe pseudolosową,
-#     oparta na rozkładzie jednorodnym
-    
-#     Argumenty:
-#     bottom - dolna granica przedziału losowania liczby
-#     top - górna granica przedziału losowania liczby
-    
-#     Return:
-#     Funckja zwraca liczbę pseudolosową z podanego przedziału
-#     """
-#     bottom=float(bottom)
-#     top=float(top)
-#     randomNumber=random.uniform(bottom, top)
-#     return randomNumber
 
 def RandomNumberGeneratorNeumann(bottom, top):
     """
@@ -99,23 +61,6 @@ def RandomNumberGeneratorNeumann(bottom, top):
     randomNumber=random.randrange(bottom, top)
     return randomNumber
    
-# def _RandomNumberGeneratorNeumann(bottom, top):
-#     """
-#     Funkcja generująca liczbe pseudolosową,
-#     oparta na moetodzie eliminacji von Neumann'a
-    
-#     Argumenty:
-#     bottom - dolna granica przedziału losowania liczby
-#     top - górna granica przedziału losowania liczby
-    
-#     Return:
-#     Funckja zwraca liczbę pseudolosową z podanego przedziału
-#     """
-#     bottom=float(bottom)
-#     top=float(top)
-#     randomNumber=random.randrange(bottom, top)
-#     return randomNumber
-
 
 @click.command()
 @click.option('--rodzaj', prompt="Podaj rodzaj operacji")
